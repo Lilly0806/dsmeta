08f7c37648b28eaa87d976f98505425e378c48ca
Lidia <lidi_1610@yahoo.com.br>
2022-07-11 14:22:49 -0300

    Other components

---
+++ frontend/src/componets/Header/index.tsx
@@ -0,0 +1,20 @@
+import logo from '../../assets/img/logo.svg'
+
+import './styles.css'
+
+function Header() {
+    return (
+        <header>
+            <div className="dsmeta-logo-container">
+                <img src={logo} alt="DsMeta" />
+                <h1>DsMeta</h1>
+                <p>Desenvolvido por
+                    <a href="https://github.com/Lilly0806"> Lidia </a>
+                </p>
+            </div>
+        </header>
+    )
+
+}
+
+export default Header
